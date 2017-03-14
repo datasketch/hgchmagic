3eb2442ec418ee159de80011ab33c4979a41001c
diftrujillo1 <diftrujillo1@gmail.com>
2017-03-14 18:17:08 -0500

    hgchFtype errors fixed

--- R/available.R
+++ R/available.R
@@ -37,16 +37,37 @@ vizList <- function(){
 #' @export
 hgchFtype <- function(hgch = NULL){
   db <- tools::Rd_db("hgchmagic")
+  # db <- db[grepl("^hgch_.*$",names(db))]
+  # meta <- lapply(db, tools:::.Rd_get_section, "section")
+  # i <<- 1
+  # cleanFtypeDoc <- function(ftype){
+  #   message(i)
+  #   i <<- i + 1
+  #   if(identical(ftype[[1]], list())) return()
+  #   ftype <- as.character(ftype[[1]])
+  #   if(identical(ftype, character(0))) return(0)
+  #   ftype <- ftype[length(ftype)-1]
+  #   gsub(" |\n","",ftype)
+  # }
+  # meta <- lapply(meta,cleanFtypeDoc)
+  # names(meta) <- gsub(".Rd","",names(meta))
+  # if(!is.null(hgch)) return(meta[[hgch]])
+  # meta
+
   db <- db[grepl("^hgch_.*$",names(db))]
-  meta <- lapply(db, tools:::.Rd_get_section, "section")
-  cleanFtypeDoc <- function(ftype){
-    ftype <- as.character(ftype[[2]][[2]])
+  names(db) <- gsub(".Rd","",names(db))
+  f <- function(dbi) {
+    # dbi <- db[[i-1]]
+    x <- as.character(dbi)
+    xx <- str_extract(x, " (.*)\n")
+    xxx <- xx[grepl("-|Ca|Ye|Nu|Da|NuP", xx)]
+    xxx <- xxx[!duplicated(xxx)]
+    ftype <- as.character(paste(xxx, collapse = ", "))
     strsplit(gsub(" |\n","",ftype),",")[[1]]
   }
-  meta <- lapply(meta,cleanFtypeDoc)
-  names(meta) <- gsub(".Rd","",names(meta))
-  if(!is.null(hgch)) return(meta[[hgch]])
-  meta
+  results <- purrr::map(db,f)
+  names(results) <- gsub(".Rd","",names(results))
+  return(results)
 }
 
 #' @export
