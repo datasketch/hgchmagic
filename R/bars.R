d9963c25db63c8859c6feb758fd23b067239b95f
CamilaAchury <camilaachury.4@gmail.com>
2017-06-12 12:44:33 -0500

    added count function

--- R/bars.R
+++ R/bars.R
@@ -1,3 +1,16 @@
+#' #'@name count_pl
+#' #'@export
+ count_pl <- function(x) {
+   if(is.na(x)){return(0)}
+
+   if ((x %% 1) != 0) {
+     nchar(strsplit(sub('0+$', '', as.character(x)), ".", fixed=TRUE)[[1]][[2]])
+   } else {
+     return(0)
+   }
+ }
+
+
 #' Vertical bar
 #' @name hgch_bar_ver_Cat
 #' @param x A data.frame
