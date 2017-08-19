675673116611f327a5751a89a58ed645244d40c9
brazadas <anaherand@hotmail.com>
2017-08-18 19:34:26 -0500

    fixing hgchMeta

--- inst/meta.R
+++ inst/meta.R
@@ -34,20 +34,24 @@ metaStatus <- data.frame("name" = c("hgch_area_DatNum", "hgch_area_CatNum", "hgc
                                     "hgch_bar_hor_top_CatNum", "hgch_bar_grouped_ver_CatCatNum", "hgch_bar_grouped_ver_CatYeaNum",
                                     "hgch_bar_grouped_hor_CatCatNum", "hgch_bar_stacked_ver_CatCatNum", "hgch_bar_stacked_100_ver_CatCatNum",
                                     "hgch_bar_stacked_hor_CatCatNum", "hgch_bar_stacked_100_hor_CatCatNum", "hgch_bar_stacked_ver_CatDatNum",
-                                    "hgch_bar_stacked_100_ver_CatDatNum", "hgch_bar_grouped_ver_CatNumP","hgch_bar_grouped_hor_CatNumP",
-                                    "hgch_bar_ver_NumP", "hgch_waterfall_CatNum", "hgch_bar_grouped_ver_CatCat", "hgch_bar_grouped_hor_CatCat",
+                                    "hgch_bar_stacked_100_ver_CatDatNum", "hgch_bar_grouped_ver_CatNum", "hgch_bar_grouped_ver_CatNumNum",
+                                    "hgch_bar_grouped_ver_CatNumNumNum", "hgch_bar_grouped_ver_CatNumNumNumNum", "hgch_bar_grouped_hor_CatNum",
+                                    "hgch_bar_grouped_hor_CatNumNum", "hgch_bar_grouped_hor_CatNumNumNum", "hgch_bar_grouped_hor_CatNumNumNumNum",
+                                    "hgch_waterfall_CatNum", "hgch_bar_grouped_ver_CatCat", "hgch_bar_grouped_hor_CatCat",
                                     "hgch_bar_stacked_ver_CatCat", "hgch_bar_stacked_hor_CatCat", "hgch_bar_stacked_100_ver_CatCat",
                                     "hgch_bar_stacked_100_hor_CatCat", "hgch_circular_bar_CatNum",
 
                                     "hgch_line_DatNum", "hgch_line_CatNum", "hgch_line_CatYeaNum", "hgch_line_YeaNum", "hgch_line_CatCatNum",
-                                    "hgch_line_CatDatNum", "hgch_2yline_YeaNumNum", "hgch_multilines_YeaNumP", "hgch_slope_CatYeaNum "),
+                                    "hgch_line_CatDatNum", "hgch_2yline_YeaNumNum", "hgch_multilines_YeaNum", "hgch_multilines_YeaNumNum",
+                                    "hgch_multilines_YeaNumNumNum", "hgch_multilines_YeaNumNumNumNum", "hgch_slope_CatYeaNum "),
 
                          "status" = c("OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", NA, NA,
 
                                       "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK",
-                                      "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK",
+                                      "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK",
+                                      "OK",
 
-                                      "OK", "OK", "OK", "OK", "OK", "OK", NA, "OK", NA),
+                                      "OK", "OK", "OK", "OK", "OK", "OK", NA, "OK", "OK", "OK", "OK", NA),
 
                          "coments" = c("Esta gráfica no sirve cuando hay un sólo dato. Se podrían agregar los marcadores cuando el número de observaciones es pequeño.
                 El eje 'y' debería centrarse en 0 (cuando hay valores negativos). Argumento que controle los separadores decimales y de miles en las variables numéricas.",
@@ -97,9 +101,9 @@ metaStatus <- data.frame("name" = c("hgch_area_DatNum", "hgch_area_CatNum", "hgc
                                        "El eje 'y' debería centrarse en 0 (cuando hay valores negativos). Argumento que controle los separadores decimales y de miles en las variables numéricas.",
                                        "El eje 'y' debería centrarse en 0 (cuando hay valores negativos). Argumento que controle los separadores decimales y de miles en las variables numéricas.",
                                        "Sólo sirve para variables con pocos niveles categóricos",
-                                       NA, NA, NA, NA, NA, NA, NA,
+                                       NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA,
 
-                                       "Ordenar los años", "Ordenar los años", "Ordenar los años", "Ordenar los años", "Ordenar los años", NA, NA, NA, NA)
+                                       "Ordenar los años", "Ordenar los años", "Ordenar los años", "Ordenar los años", "Ordenar los años", NA, NA, NA, NA, NA, NA, NA)
 )
 metaStatus <- left_join(select(hgchmagic::hgchMeta(), name), metaStatus, by = name)
 write_csv(metaStatus, "inst/meta-status.csv")
