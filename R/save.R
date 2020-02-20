2d32af1b99f6cdb59c5d44eba0812e8908f7308f
brazadas <anaherand@hotmail.com>
2020-02-19 20:39:05 -0500

    save_hgchmagic

--- R/save.R
+++ R/save.R
@@ -1,20 +1,20 @@
 #' save
 #' @export
-save_hgchmagic <- function(viz,
-                           filename,
-                           format = NULL,
-                           width = 660,
-                           height = 500, ...) {
-  format <- file_ext(filename) %||% "png"
-  tmp <- paste(tempdir(), 'html', sep ='.')
+save_hgchmagic <- function (viz, filename, format = NULL, width = 660, height = 500, ...) {
+  if (is.null(format)) {
+    format <- file_ext(filename) %||% "png"
+  }
+  filename <- file_path_sans_ext(filename)
+  tmp <- paste(tempdir(), "html", sep = ".")
   htmltools::save_html(viz, tmp)
   tmpSave <- filename
-  if (format == 'html') {
-    htmlwidgets::saveWidget(viz, filename)
-  } else {
-    webshot::webshot(tmp, filename, vwidth = width, vheight = height)
+  if (format == "html") {
+    htmlwidgets::saveWidget(viz, paste0(filename, ".", format))
+  }
+  else {
+    webshot::webshot(tmp, paste0(filename, ".", format),
+                     vwidth = width, vheight = height)
   }
   file.copy(filename, filename)
-
 }
 
