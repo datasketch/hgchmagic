2279fa559805481c6cbbd1e61c3b19a2e049eff4
Camila Achury <camilaachury.4@gmail.com>
2021-02-08 14:37:09 -0300

    add description of titles in chart options

--- R/hgch-viz-opts.R
+++ R/hgch-viz-opts.R
@@ -2,7 +2,12 @@
 #'
 #' @description
 #' This page describes the `<chart-options>` arguments
-#'
+#' @param title String with the main title (on top). Default is NULL
+#' @param subtitle String for subtitle (below the title). Default is NULL
+#' @param caption String for caption (bottom). Default is NULL
+#' @param hor_title String. The main title (on top). Default is NULL
+#' @param ver_title String. The main title (on top). Default is NULL
+#' @param legend_title String. The main title (on top). Default is NULL
 #' @keywords internal
 #' @name hgch_viz_options
 NULL
