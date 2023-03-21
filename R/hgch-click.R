3574785c646788d32b670d5e242f8737b11cdfd9
CamilaAchuri <camilaachury.4@gmail.com>
2023-03-21 19:13:10 -0300

    click treemap function

--- R/hgch-click.R
+++ R/hgch-click.R
@@ -4,12 +4,13 @@ click_functions <- function(viz, frtype = NULL, id_click = NULL) {
   click_func <- paste0("function(event) {Shiny.onInputChange('", id_click, "', {id:event.point.name, timestamp: new Date().getTime()});}")
 
   if (frtype %in% c("CatCatNum", "CatCat", "CatDat", "CatDatNum", "DatNumNum")) {
-    click_func <- paste0("function(event) {Shiny.onInputChange('", id_click, "', {cat:this.name, id:event.point.category, timestamp: new Date().getTime()});}")
+    click_func <- paste0("function(event) {Shiny.onInputChange('", id_click, "', {cat:event.point.category, id:this.name, timestamp: new Date().getTime()});}")
   }
+
 
   if (viz %in% "treemap") {
     if (frtype %in% c("CatCatNum", "CatCat")) {
-    click_func <- paste0("function(event) {Shiny.onInputChange('", id_click, "', {cat:event.point.node, id:event.point.name, timestamp: new Date().getTime()});}")
+    click_func <- paste0("function(event) {Shiny.onInputChange('", id_click, "', {cat:event.point.node.parent, id:event.point.node.name, timestamp: new Date().getTime()});}")
     }
   }
 
